73c29d48019c63a2a67a49b6be20d3cd4d4ca872
Bora Sevim <sevimbora@gmail.com>
2024-07-19 15:08:09 +0300

    Two Sum solved - 19.07.2024

---
+++ Two_Sum-19.07.2024/two_sum.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                    return [i, j];
+            }
+        }
+        return null;
+    }
+}
