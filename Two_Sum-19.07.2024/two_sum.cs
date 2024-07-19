b1cdbf5870892cb83afbaf09883dec859028e5e7
Bora Sevim <sevimbora@gmail.com>
2024-07-19 15:09:42 +0300

    Fix two_sum.cs

--- Two_Sum-19.07.2024/two_sum.cs
+++ Two_Sum-19.07.2024/two_sum.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         for(int i = 0; i < nums.Length; i++)
         {
-            for(int j = 1; j < nums.Length; j++)
+            for(int j = i + 1; j < nums.Length; j++)
             {
                 if(nums[i] + nums[j] == target)
                     return [i, j];
